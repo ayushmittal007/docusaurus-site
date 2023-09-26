e67954ad2f09e0d89bf07c867285fe4abdfc31fb
Ishika Gupta <apple@apples-MacBook-Air-2.local>
2023-09-26 14:11:41 +0530

    deploying with netlify

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,17 +11,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://IshikaGupta3112.github.io',
+  url: 'https://docusaurus-2.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus-site/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'IshikaGupta3112', // Usually your GitHub org/user name.
-  projectName: 'docusaurus-site', // Usually your repo name.
-  deploymentBranch:'gh-pages',
-  trailingSlash:false,
+  // organizationName: 'IshikaGupta3112', // Usually your GitHub org/user name.
+  // projectName: 'docusaurus-site', // Usually your repo name.
+  // deploymentBranch:'gh-pages',
+  // trailingSlash:false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
