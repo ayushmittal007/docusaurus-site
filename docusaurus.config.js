713ef41a12718d31ec07064e4fd80598b75419aa
Ishika Gupta <113205098+IshikaGupta3112@users.noreply.github.com>
2023-09-29 00:34:03 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,7 +103,7 @@ const config = {
           },
           {
             label: 'Manya Aggarwal',
-            to: '/docs/manya_aggarwal',
+            to: '/docs/Manya',
             position: 'left'
           },
           // {
